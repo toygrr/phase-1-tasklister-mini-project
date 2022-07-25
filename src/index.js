473f7bd6c7f6786c40e7d2797bc475e3163176c0
Tyler <toygrrr@gmail.com>
2022-07-24 21:04:45 -0700

    i did it?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(e.target.text_input.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", toDoDelete);
+  btn.textContent = "x";
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function toDoDelete(e) {
+  e.target.parentNode.remove();
+}
